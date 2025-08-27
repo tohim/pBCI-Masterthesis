877d1fa8e0167a0cc6cf899d15c1a4e970bc9c3b
tohim <66789389+tohim@users.noreply.github.com>
2025-08-27 18:30:32 +0200

    Update run_within_dataset.m

--- Offline/Offline Pipeline Functions/run_within_dataset.m
+++ Offline/Offline Pipeline Functions/run_within_dataset.m
@@ -3,13 +3,6 @@ function [acc, acc_hyper, acc_norm, acc_hyper_norm, pct_std, pct_hyper, pct_norm
     % Load Data
     [eeg_data, labels] = load_processed_data(opts);
     [train_epochs, val_epochs, test_epochs, train_labels, val_labels, test_labels] = split_data(eeg_data, labels, opts);
-    save([opts.dataset, '_QUICK_train_epochs.mat'], 'train_epochs');
-    save([opts.dataset, '_QUICK_val_epochs.mat'], 'val_epochs');
-    save([opts.dataset, '_QUICK_test_epochs.mat'], 'test_epochs');
-    save([opts.dataset, '_QUICK_train_labels.mat'], 'train_labels');
-    save([opts.dataset, '_QUICK_val_labels.mat'], 'val_labels');
-    save([opts.dataset, '_QUICK_test_labels.mat'], 'test_labels');
-
 
     % Extract Features
     [train_features, val_features, test_features, W_csp] = extract_all_features(train_labels, val_labels, test_labels, train_epochs, val_epochs, test_epochs, opts);
@@ -20,5 +13,7 @@ function [acc, acc_hyper, acc_norm, acc_hyper_norm, pct_std, pct_hyper, pct_norm
     round(acc_hyper);
     round(acc_norm);
     round(acc_hyper_norm);
+    
 end
+
 
